3e575dc4da483bcec3f7a6b7153605cbf2f416ae
Dave <dave.bird@dsl.pipex.com>
2019-09-26 12:01:49 +0100

    tidy up comments

--- pressure.c
+++ pressure.c
@@ -51,23 +51,23 @@ int main(void)
     uint8_t status = 0;
 
     /* open i2c comms */
-    if ((fd = open(DEV_PATH, O_RDWR)) < 0) {
-	perror("Unable to open i2c device");
-	exit(1);
+    if((fd = open(DEV_PATH, O_RDWR)) < 0) {
+        perror("Unable to open i2c device");
+        exit(1);
     }
 
     /* configure i2c slave */
-    if (ioctl(fd, I2C_SLAVE, DEV_ID) < 0) {
-	perror("Unable to configure i2c slave device");
-	close(fd);
-	exit(1);
+    if(ioctl(fd, I2C_SLAVE, DEV_ID) < 0) {
+        perror("Unable to configure i2c slave device");
+        close(fd);
+        exit(1);
     }
 
     /* check we are who we should be */
-    if (i2c_smbus_read_byte_data(fd, WHO_AM_I) != 0xBD) {
-	printf("%s\n", "who_am_i error");
-	close(fd);
-	exit(1);
+    if(i2c_smbus_read_byte_data(fd, WHO_AM_I) != 0xBD) {
+        printf("%s\n", "who_am_i error");
+        close(fd);
+        exit(1);
     }
 
     /* Power down the device (clean start) */
@@ -77,15 +77,15 @@ int main(void)
     i2c_smbus_write_byte_data(fd, CTRL_REG1, 0x84);
 
     /* Run one-shot measurement (temperature and pressure), the set bit will be reset by the
-     * sensor itself after execution (self-clearing bit) */
+     * sensor itself after execution (self-clearing bit)
+     */
     i2c_smbus_write_byte_data(fd, CTRL_REG2, 0x01);
 
     /* Wait until the measurement is complete */
     do {
-	delay(25);		/* 25 milliseconds */
-	status = i2c_smbus_read_byte_data(fd, CTRL_REG2);
-    }
-    while (status != 0);
+        delay(25);      /* 25 milliseconds */
+        status = i2c_smbus_read_byte_data(fd, CTRL_REG2);
+    } while(status != 0);
 
     /* Read the temperature measurement (2 bytes to read) */
     temp_out_l = i2c_smbus_read_byte_data(fd, TEMP_OUT_L);
